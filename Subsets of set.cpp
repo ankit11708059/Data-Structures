5a41450050a5e9e1e19516abb4f8107d020d1b1d
Ankit Sharma <35391426+ankit11708059@users.noreply.github.com>
2018-10-19 12:07:00 +0530

    Update Subsets of set.cpp

--- Subsets of set.cpp
+++ Subsets of set.cpp
@@ -1,30 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/* Here the concept is that we know that a set has 2 raised to n subset if their are
-n elements in it so here we extract the each upto 2 raised to n if it is set then print
-corresponding character and right shift that number*/
-void subsets(int no,string s)
+void sub(char *a,char *k,int i,int j)
 {
-	int i=0;
-	while(no>0)
+	if(a[i]=='\0')
 	{
-		if(no&1)
-		cout<<s[i];
-		else
-		cout<<"";
-		no=no>>1;
-		i++;
+		k[j]='\0';
+	
+			cout<<k<<",";
+			return;
 	}
-	cout<<endl;
+	k[j]=a[i];
+	sub(a,k,i+1,j+1);
+	sub(a,k,i+1,j);
 }
 int main()
 {
-	string s;
-	cin>>s;
-	int k=pow(2,s.size());
-	for(int i=0;i<k;i++)
-	{
-		subsets(i,s);
-	}
+	char a[100],k[100];
+	cin>>a;
+	sub(a,k,0,0);
+	
 }
