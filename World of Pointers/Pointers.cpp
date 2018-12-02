f9335cfbc83a8662a329c439e1391d3b04c27bde
Ankit Sharma <35391426+ankit11708059@users.noreply.github.com>
2018-12-02 21:04:17 +0530

    Update Pointers.cpp

--- World of Pointers/Pointers.cpp
+++ World of Pointers/Pointers.cpp
@@ -27,3 +27,19 @@ it like we write *p.
    Now we know that we have a pointer variable and we want to increment the value present inside the address so first we derefrence
    it and then add 1 like // *(p) + 1
    But if we simply do this p+1 then if my p has address of 400 it will be incremented by 4 and become 404.
+ 
+         
+    *****************************************************************************************************************************
+    *****************************************************************************************************************************
+                                             ARRAY AND POINTERS
+    *****************************************************************************************************************************
+    *****************************************************************************************************************************
+    Let us suppose that we have initialised a array as // a[3]={1,2,3};
+    Now we know that the we can can access the array value as a[0] here we will get first value of the array
+    But if we cout just only a then we get the address of first index and if we derefrence a then we would get the value at first
+    index. We Just cout &a also then we will get the address of first value.
+      
+      
+      
+     ***************************************************************************************************************************
+     ***************************************************************************************************************************
